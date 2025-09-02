92ec313d28dfad46f62b5161e40f3870dd197149
mixa63 <183983866+mixa63@users.noreply.github.com>
2025-09-03 01:07:08 +0200

    Dodaj nedostajuce importe u kontrolerima

--- backend/app/Http/Controllers/LevelController.php
+++ backend/app/Http/Controllers/LevelController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Models\Level;
+use App\Models\UserLevel;
 
 class LevelController extends Controller
 {
